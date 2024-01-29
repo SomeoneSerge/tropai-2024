597f1a6f087854d64e67048df3ed4c8ab117596d
Someone Serge <sergei.kozlukov@aalto.fi>
2024-01-29 19:43:45 +0000

    nix: fix: the nb actually refers to pypi/graphviz

--- nix/mk-shell.nix
+++ nix/mk-shell.nix
@@ -1,8 +1,9 @@
-{ python3Packages, graphviz, mkShell }:
+{ lib, python3Packages, mkShell }:
 
 mkShell {
   packages = [
     (python3Packages.python.withPackages (ps: with ps; [
+      graphviz
       ipykernel
       notebook
       pyro-ppl
@@ -11,7 +12,6 @@ mkShell {
       torch
       torchvision
     ]))
-    graphviz
   ];
   shellHook = ''
     python3 -m ipykernel install --user --name "tropai-2024" --display-name "Tropcal Probabilistic AI 2024"
